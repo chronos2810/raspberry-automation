fb6bc92b49c1f4ca3ab70bab6691215b70f959b9
chronos2810 <hgpaz.2810@gmail.com>
2021-11-19 16:43:52 -0300

    Checkpoint

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 Raspberry Pi Ansible and Docker files
 
+- Usage:
+
 ```bash
 # Change default password
 passwd
 
 # Clone the repo
 cd ~
-sudo apt install git
+sudo apt update && sudo apt install git
 git clone https://github.com/chronos2810/raspberry-automation.git
 
 # Run the scripts
 cd raspberry-automation
-ansible/rpi-bootstrap/run.sh
+rpi-bootstrap/run.sh
 docker/run-docker-compose.sh
 ```
--- rpi-bootstrap/run.sh
+++ rpi-bootstrap/run.sh
@@ -5,6 +5,6 @@ set -x
 sudo apt install ansible cowsay -y
 
 mkdir -p ~/.ansible/roles
-ln -s ~/ansible/rpi-bootstrap ~/.ansible/roles/rpi-bootstrap
+ln -s ~/raspberry-automation/rpi-bootstrap ~/.ansible/roles/rpi-bootstrap
 
-ansible-playbook -i ~/ansible/rpi-bootstrap/tests/inventory ~/ansible/rpi-bootstrap/tests/playbook.yml --ask-become-pass
+ansible-playbook -i ~/raspberry-automation/rpi-bootstrap/tests/inventory ~/raspberry-automation/rpi-bootstrap/tests/playbook.yml --ask-become-pass
