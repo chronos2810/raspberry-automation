211f8006ff1c4d4f86943c8a2e77452965f6ad24
chronos2810 <hgpaz.2810@gmail.com>
2021-11-19 17:03:26 -0300

    Checkpoint

--- docker/run-docker-compose.sh
+++ docker/run-docker-compose.sh
@@ -3,7 +3,9 @@
 set -x
 cd ~/raspberry-automation/docker/pi-hole
 docker-compose up -d
+docker-compose ps
 
 cd ~/raspberry-automation/docker/samba
 mkdir ~/media
 docker-compose up -d
+docker-compose ps
