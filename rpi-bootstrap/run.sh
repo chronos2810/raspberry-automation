950f1bde004e9c6f93ad5060a7d8da2fae18776f
chronos2810 <hgpaz.2810@gmail.com>
2023-07-12 16:43:30 -0400

    Checkpoint

--- rpi-bootstrap/run.sh
+++ rpi-bootstrap/run.sh
@@ -6,6 +6,6 @@ sudo apt install ansible cowsay -y
 sudo apt autoremove -y
 
 mkdir -p ~/.ansible/roles
-ln -s ~/raspberry-automation/rpi-bootstrap ~/.ansible/roles/rpi-bootstrap
+ln -s ~/coding-repos/raspberry-automation/rpi-bootstrap ~/.ansible/roles/rpi-bootstrap
 
-ansible-playbook -i ~/raspberry-automation/rpi-bootstrap/tests/inventory ~/raspberry-automation/rpi-bootstrap/tests/playbook.yml --ask-become-pass
+ansible-playbook -i ~/coding-repos/raspberry-automation/rpi-bootstrap/tests/inventory ~/coding-repos/raspberry-automation/rpi-bootstrap/tests/playbook.yml --ask-become-pass
