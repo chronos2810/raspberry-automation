c0aabd3b388bbf4f92396e1ceaebbf9c4cca7933
chronos2810 <hgpaz.2810@gmail.com>
2023-11-20 18:28:40 -0300

    Checkpoint

--- docker/wake-on-lan-flask-svc/src/app.py
+++ docker/wake-on-lan-flask-svc/src/app.py
@@ -1,14 +1,23 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/run')
 def run():
-    return 'Run!'
+    # os.system('cmd /k "C:\Windows\System32\DisplaySwitch.exe /external"')
+    # os.system('cmd /k "C:\Program Files (x86)\Steam\Steam.exe -gamepadui"')
+    # os.system('start "Steam" /b "steam://open/bigpicture"')
+    return "test"
 
-@app.route('/shutdown')
+
+@app.route('/stop')
 def shutdown():
-    return 'Shutdown!'
+    os.system('cmd /k "C:\Windows\System32\DisplaySwitch.exe /internal"')
+    os.system('cmd /k "taskkill.exe /F /IM Steam.exe /T"')
+    return "asdf"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
